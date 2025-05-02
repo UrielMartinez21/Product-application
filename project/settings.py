4b26fc656c6f01ac813947c36edd4d747f1f601f
UrielMartinez21 <uriellmartinezz35@gmail.com>
2025-05-01 19:00:27 -0600

    feat(all project): updated database

--- project/settings.py
+++ project/settings.py
@@ -42,8 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.sitemaps',
-    # my apps
+    # Third-party apps
     'taggit',
+    # My apps
     'product.apps.ProductConfig',
 ]
 
@@ -83,8 +84,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', default='3306'),
     }
 }
 
